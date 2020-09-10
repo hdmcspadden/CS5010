201a39ff18037f9684493200e1ab8e29f422bedb
hdmcspadden <hdmcspadden@gmail.com>
2020-09-10 11:16:06 -0400

    Working on live session exercise

---
+++ Module03/Exam1StudyGuide.py
@@ -0,0 +1,146 @@
+# -*- coding: utf-8 -*-
+"""
+CS 5010: EXAM 1 Study Guide
+
+Created on Wed Sep  9 16:00:41 2020
+
+@author: Diana McSpadden
+"""
+
+# Data 
+'''
+What is data?
+    Data is raw, unprocessed, and uninterpreted information
+    It is an abstract concept
+    
+    Set of qualitative and quantitative values
+    
+    My answer: Data is an attempt by humans to count, measure, and categorize the world around us to make associations and predictions and make sense of the human condition.
+    
+    
+Different types of data
+    Qualitative
+        Descriptive information (relates to the quality of something)
+        Example: Nae: Edgar Codd
+
+    Quantitative data
+
+        Discrete data
+            Numerical information
+            Can only take certain values (whole numbers, “counted”)
+            An example: Year Born: 1980
+        
+        Continuous data
+            Numerical information
+            Continuous range (often “measured”)
+            Example: Size: 227.0 cm
+            
+        Categorical
+            A limited, and usually fixed, number of possible values to describe an item 
+            (giving an item a “label” or placing into a “category”)
+            Blood type is an example.
+            Example: "Role: Drummer"
+            Example: Year Born: 1980
+            
+Properties of data
+
+    Source – where did the data come from? Trustworthy? Unbiased? Reliable? Sufficient?
+
+    Quality – data is maintained and data is clean
+        Sometimes means no missing data, correct labels, data types, and data in useable format.
+
+    Scale – data is representative?
+        Is there enough data to draw meaningful conclusions?
+        Is it representative of the domain in question
+        Does more data need to be obtained? Can it be obtained?
+
+    Variety – different types of data
+        Can it provide, for example, offline, online, personal,
+        anonymous, mobile, PC, branded, blended, demo, interest
+        and/or intent data?
+
+        Depending on context/situation this may be on of the
+        more important factors…
+
+    Value of data is extracted through context and presentation
+        Case studies (case studies are used to provide context, while no direct questions on these will be asked on the exam, you can use them to provide examples to support your solutions)
+            Tufte’s redesign of the Space Shuttle Challenger data
+                Bad data: looked like crayons in box
+                Redesign: number damaged o-rings by temperature
+            
+            Data presentation on the consumption of sugary soda
+                Context was important - eed to jog 50 minutes to burn calories of one soda
+
+    Context and presentation increases understanding and can change behavior/attitude. 
+        Caution: careful not to mislead/misinform!
+
+    Data → Information → Knowledge
+        What each of them are; difference between each of them
+        "Drowning in Data, Starving for Information" - Module0 PDF
+        
+        Data: Uninterpreted information.
+            
+        Information: 
+            contextualized data, 
+            finding patterns
+            associations
+            relationships
+            performing calculations on data
+            
+        KNowledge:
+            Derived from extensive amounts of experience dealing with
+            information that has been learned, analyzed, applied,
+            and understood.
+            
+            historical patterns
+            
+    Data products
+        Case studies 
+            (case studies are used to provide context, 
+             while no direct questions on these will be asked on the exam, 
+             you can use them to provide examples to support your solutions)
+
+        CD Database: A data product because new value was created
+            from existing information. More data was generated
+            as a result of the original data.
+
+        Google Flu Tracking
+            google searches on flu-related topics.
+            Google spotted Swine Flu 2 weeks earlier than CDC
+
+'''
+
+
+# GENERAL PYTHON CODING
+'''
+a.Be able to read code
+b. Be able to show the output of a piece of code
+c. Be able to compare two pieces of code
+d. Describe the (many!) benefits of applying object-oriented principles
+BENEFITS OF OOP
+    * reusability
+    * inheritance
+    * consistency in the code abstraction of actual "object"
+
+e. Describe what the major data structures are and their properties (and scenarios in
+which they would be best suited – e.g. lists, dictionaries, and tuples)
+    list
+    
+    dictionary
+
+    tuple   
+    
+    int, long, bool, float, string, 
+    list (mutable) ['dot','dash']; [], 
+    tuple (immutable) ('dot', 'dash'); (); ("just one",), 
+    dict {'key1':1, 'key2': 2}; {}, 
+    file
+
+f. Decide on an appropriate data structure(s) to use given the scenario
+g. Correctly implement the appropriate data structure(s) for the given scenario and
+manipulate data within this structure correctly and appropriately
+h. Describe the major functions used and their properties (and scenarios in which they
+would be best suited)
+i. Explain some of the elements that results in “good quality” code
+
+'''
--- Module03/Module03LiveSession.py
+++ Module03/Module03LiveSession.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Sep  4 12:29:59 2020
+Name of Activity: Module 03: Live Session Exercise: Exception Handling
+Name: Diana McSpadden
+UID: hdm5s
 
 @author: Diana McSpadden
 
@@ -20,53 +22,91 @@ no "except" block is unreachable.
 Test your code out by running it multiple times and allowing your code to encounter various kinds of exceptions (you can do this by incorporating user input or otherwise.) Have unique bodies of code in each of the "except" blocks (at the very least, different print() statements), and run your code enough times and force your code to encounter the various kinds of exceptions such that output from EACH "except" block is printed, proving that no "except" block is unreachable.
 """
 
+i = 0
+j = 0
+k = 0
+m = 0
+n = 0
+p = 0
+q = 0
+
 def exceptionGauntlet():
+    '''
+    I want to throw each error once. In order to test, 
+    you will need to generate each error one time to not be prompted with the "input" statement again.
+    '''
+    global i 
+    global j
+    global k
+    global m
+    global n
+    global p
+    global q
+    
     output = None
+    
     try:
         # try to get a type error
-        #  couldn't figure out how to use user input to generate this
+        if (i == 0):
+            raise TypeError("This is a Type exception.")
         
         # try to get a value error
-        inputValueError = input("I am going to try to make this a number: ")
-        tryNumber = int(inputValueError)
+        if (j == 0):
+            inputValueError = input("I am going to try to make this a number: ")
+            tryNumber = int(inputValueError)
         
         # try to get a zero division error
-        inputZeroDivError = input("Divide by what number? ")
-        tryZeroDiv = 2 / int(inputZeroDivError)
+        if (k == 0):
+            inputZeroDivError = input("Divide by what number? ")
+            tryZeroDiv = 2 / int(inputZeroDivError)
         
         # try to get an arithmetic error
-        
+        if (m == 0):
+            raise ArithmeticError("This is an arithmetic exception.")
         
         # try to get a file not found error
-        inputFileName = input("What file should we open? ")
-        file = open(inputFileName, 'r')
-        file.close()
+        if (n == 0):
+            inputFileName = input("What file should we open? ")
+            file = open(inputFileName, 'r')
+            file.close()
         
         # try to get an IO Error
+        if (p == 0):
+            raise IOError("This is an IO/OS exception.")
         
-        # try to get a general Exception
+        # try to get a General Exception
+        if (q == 0):
+            raise Exception("This is a general exception.")
         
     
+    # when each error is caught in its except, set the output variable to the text of the error
     except TypeError as errType:
-        output = errType
+        i += 1 # increment i
+        output = "This is a Type Error: " + str(errType)
     
     except ValueError as errValue:
-        output = errValue
+        j += 1 # increment j
+        output = "This is a Value Error: " + str(errValue)
         
     except ZeroDivisionError as errZeroDivision:
-        output = errZeroDivision
+        k += 1 # increment k
+        output = "This is a Divide by Zero Error: " + str(errZeroDivision)
     
     except ArithmeticError as errArithmetic:
-        output = errArithmetic
+        m += 1 # increment m
+        output = "This is an Arithmetic Error: " + str(errArithmetic)
         
     except FileNotFoundError as errFileNotFound:
-        output = errFileNotFound
+        n += 1 # increment n
+        output = "This is a File Not Found Error: " + str(errFileNotFound)
     
     except IOError as errIO:
-        output = errIO.message
+        p += 1 # increment p
+        output = "This is an IO/OS Error: " + str(errIO)
     
     except Exception as errException:
-        output = errException
+        q += 1 # increment q
+        output = "This is a General Exception: " + str(errException)
     finally:
         if (output == None):
             print("You completed the Exception Gauntlet")
